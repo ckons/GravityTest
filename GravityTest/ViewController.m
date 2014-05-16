4c1728160c1a70981605dc2d6b96ed933cd67a08
Dino <dinokonstantakis@dinos-macbook-pro.local>
2014-05-15 20:11:01 -0700

    Initial Commit

---
+++ GravityTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GravityTest
+//
+//  Created by Dino on 5/15/14.
+//  Copyright (c) 2014 Constantine. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ GravityTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GravityTest
+//
+//  Created by Dino on 5/15/14.
+//  Copyright (c) 2014 Constantine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
